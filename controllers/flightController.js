9ff70cb1735d0211947ca6fa221a0f727f6fa4ae
anajembaedwin <anajembaedwin@gmail.com>
2022-11-05 06:28:33 +0100

    setup flight controller

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,14 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
+const Flights = require("../models/Flight");
+
+
+// get all flights
+exports.getFlights = async (req, res) => {
+    try {
+        const flights = Flights;
+        res.status(200).json(flights);
+    } catch (err) {
+        res.status(500).json({message: err});
+    }
 }
 
 
